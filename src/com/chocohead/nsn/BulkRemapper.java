56feeb3055c7c663fad8c8e72455294d236dd6f5
Chocohead <Chocohead@Aroma1997.org>
2021-07-12 20:58:53 +0100

    Run nest fixing on Minecraft itself too
    No more access issues

--- src/com/chocohead/nsn/BulkRemapper.java
+++ src/com/chocohead/nsn/BulkRemapper.java
@@ -3,6 +3,10 @@ package com.chocohead.nsn;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.lang.reflect.Modifier;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystemAlreadyExistsException;
+import java.nio.file.FileSystems;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
@@ -17,11 +21,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
-import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 
 import com.google.common.base.Verify;
@@ -61,9 +63,7 @@ import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 import org.spongepowered.asm.mixin.transformer.ext.Extensions;
 
 import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.MappingResolver;
 import net.fabricmc.loader.api.ModContainer;
-import net.fabricmc.loader.api.metadata.ModMetadata;
 
 import com.chocohead.mm.api.ClassTinkerers;
 
@@ -77,92 +77,24 @@ public class BulkRemapper implements IMixinConfigPlugin {
 
 		Set<String> toTransform = new HashSet<>(4096);
 		SetMultimap<String, ClassReader> nests = Multimaps.newSetMultimap(new HashMap<>(128), ReferenceOpenHashSet::new);
-		@SuppressWarnings("resource") //So long as we're careful we're not leaking anything
 		RecyclableDataInputStream buffer = new RecyclableDataInputStream();
 
 		for (ModContainer mod : FabricLoader.getInstance().getAllMods()) {
-			ModMetadata metadata = mod.getMetadata();
+			String modID = mod.getMetadata().getId();
 
-			if (!"fabricloader".equals(metadata.getId()) && !"java".equals(metadata.getId()) && !"nsn".equals(metadata.getId())) {
-				try {
-					Files.walkFileTree(mod.getRootPath(), new FileVisitor<Path>() {
-						private final UnaryOperator<String> remapper = "minecraft".equals(metadata.getId()) ? new UnaryOperator<String>() {
-							private final MappingResolver remapper = FabricLoader.getInstance().getMappingResolver();
+			if (!"fabricloader".equals(modID) && !"java".equals(modID) && !"nsn".equals(modID)) {
+				if ("minecraft".equals(modID)) {
+					String block = FabricLoader.getInstance().getMappingResolver().mapClassName("intermediary", "net.minecraft.class_2248").replace('.', '/');
 
-							@Override
-							public String apply(String clazz) {
-								return remapper.mapClassName("official", clazz.replace('/', '.'));
-							}
-						} : UnaryOperator.identity();
-						private final MixinChecker checker = new MixinChecker();
-
-						@Override
-						public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
-							return FileVisitResult.CONTINUE;
+					try (FileSystem fs = FileSystems.newFileSystem(BulkRemapper.class.getResource('/' + block + ".class").toURI(), Collections.emptyMap())) {
+						for (Path root : fs.getRootDirectories()) {
+							walkTree(buffer, root, toTransform, nests);
 						}
-
-						@Override
-						public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-							if ("class".equalsIgnoreCase(MoreFiles.getFileExtension(file))) {
-								try (DataInputStream in = buffer.open(Files.newInputStream(file))) {
-									in.mark(16);
-
-								    int magic = in.readInt();
-								    if (magic != 0xCAFEBABE) {
-								    	System.err.printf("Expected magic in %s but got %X%n", file, magic);
-								    	return FileVisitResult.CONTINUE; //Not a class?
-								    }
-
-								    in.readUnsignedShort(); //Minor version
-								    if (in.readUnsignedShort() > Opcodes.V1_8) {
-								    	in.reset();
-
-								    	ClassReader reader = new ClassReader(in); 
-								    	String name = reader.getClassName();
-								    	//System.out.println("Planning to transform ".concat(name));
-								    	reader.accept(checker, ClassReader.SKIP_CODE | ClassReader.SKIP_DEBUG);
-
-								    	if (!checker.isMixin()) {
-								    		toTransform.add(remapper.apply(name));
-
-								    		if (name.equals(remapper.apply(name)) && checker.inNestedSystem()) {
-									    		nests.put(checker.isNestHost() ? name : checker.getNestHost(), reader);
-									    	}								    		
-								    	} else if (Modifier.isInterface(reader.getAccess())) {
-								    		for (String target : checker.getTargets()) {
-								    			HUMBLE_INTERFACES.put(target, name);
-								    		}
-								    	}
-
-								    	checker.reset();
-								    }// else System.out.printf("Not transforming %s as its version is %d%n", MoreFiles.getNameWithoutExtension(file), version);
-								} catch (IOException e) {
-									System.err.println("Broke visiting " + file); //Oops
-									e.printStackTrace();
-								}
-							}
-
-							return FileVisitResult.CONTINUE;
-						}
-
-						@Override
-						public FileVisitResult visitFileFailed(Path file, IOException e) {
-							System.err.println("Broke trying to visit " + file); //Oops?
-							e.printStackTrace();
-							return FileVisitResult.CONTINUE;
-						}
-
-						@Override
-						public FileVisitResult postVisitDirectory(Path dir, IOException e) {
-							if (e != null) {//Oops?
-								System.err.println("Broke having visited " + dir);
-								e.printStackTrace();
-							};
-							return FileVisitResult.CONTINUE;
-						}
-					});
-				} catch (IOException e) {//This can only be thrown from the file visitor doing so
-					throw new AssertionError("Unexpected exception", e);
+					} catch (URISyntaxException | FileSystemAlreadyExistsException | IOException e) {
+						throw new RuntimeException("Failed to read Minecraft jar", e);
+					}
+				} else {
+					walkTree(buffer, mod.getRootPath(), toTransform, nests);
 				}
 			}
 		}
@@ -201,6 +133,81 @@ public class BulkRemapper implements IMixinConfigPlugin {
 		}
 	}
 
+	private static void walkTree(RecyclableDataInputStream buffer, Path jar, Set<String> toTransform, SetMultimap<String, ClassReader> nests) {
+		try {
+			Files.walkFileTree(jar, new FileVisitor<Path>() {
+				private final MixinChecker checker = new MixinChecker();
+
+				@Override
+				public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+					return FileVisitResult.CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+					if ("class".equalsIgnoreCase(MoreFiles.getFileExtension(file))) {
+						try (DataInputStream in = buffer.open(Files.newInputStream(file))) {
+							in.mark(16);
+
+							int magic = in.readInt();
+							if (magic != 0xCAFEBABE) {
+								System.err.printf("Expected magic in %s but got %X%n", file, magic);
+								return FileVisitResult.CONTINUE; //Not a class?
+							}
+
+							in.readUnsignedShort(); //Minor version
+							if (in.readUnsignedShort() > Opcodes.V1_8) {
+								in.reset();
+
+								ClassReader reader = new ClassReader(in);
+								String name = reader.getClassName();
+								//System.out.println("Planning to transform ".concat(name));
+								reader.accept(checker, ClassReader.SKIP_CODE | ClassReader.SKIP_DEBUG);
+
+								if (!checker.isMixin()) {
+									toTransform.add(name);
+
+									if (checker.inNestedSystem()) {
+										nests.put(checker.isNestHost() ? name : checker.getNestHost(), reader);
+									}
+								} else if (Modifier.isInterface(reader.getAccess())) {
+									for (String target : checker.getTargets()) {
+										HUMBLE_INTERFACES.put(target, name);
+									}
+								}
+
+								checker.reset();
+							}// else System.out.printf("Not transforming %s as its version is %d%n", MoreFiles.getNameWithoutExtension(file), version);
+						} catch (IOException e) {
+							System.err.println("Broke visiting " + file); //Oops
+							e.printStackTrace();
+						}
+					}
+
+					return FileVisitResult.CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult visitFileFailed(Path file, IOException e) {
+					System.err.println("Broke trying to visit " + file); //Oops?
+					e.printStackTrace();
+					return FileVisitResult.CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult postVisitDirectory(Path dir, IOException e) {
+					if (e != null) {//Oops?
+						System.err.println("Broke having visited " + dir);
+						e.printStackTrace();
+					};
+					return FileVisitResult.CONTINUE;
+				}
+			});
+		} catch (IOException e) {//This can only be thrown from the file visitor doing so
+			throw new AssertionError("Unexpected exception", e);
+		}
+	}
+
 	private static void generateMixin(String name, Iterable<String> targets) {
 		ClassWriter cw = new ClassWriter(0);
 		cw.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC | Opcodes.ACC_ABSTRACT | Opcodes.ACC_INTERFACE, name, null, "java/lang/Object", null);
@@ -218,46 +225,53 @@ public class BulkRemapper implements IMixinConfigPlugin {
 	private static List<Runnable> resolveNestSystem(Collection<ClassReader> system) {
 		class Member {
 			public final String name;
-			public final Set<String> members;
-			public final Set<String> wantedMethods = new HashSet<>();
-			public final Set<String> wantedFields = new HashSet<>();
-			public final Map<String, Set<String>> usedMethods;
-			public final Map<String, Set<String>> usedFields;
+			public final Set<String> methods = new HashSet<>();
+			public final Set<String> usedMethods = new HashSet<>();
+			public final Set<String> fields = new HashSet<>();
+			public final Set<String> usedFields = new HashSet<>();
 
-			Member(String name, Set<String> members, Map<String, Set<String>> usedMethods, Map<String, Set<String>> usedFields) {
+			Member(String name) {
 				this.name = name;
-				this.members = Collections.unmodifiableSet(members);
-				this.usedMethods = Collections.unmodifiableMap(usedMethods);
-				this.usedFields = Collections.unmodifiableMap(usedFields);
 			}
 		}
-		Map<String, Member> ownerToMembers = new HashMap<>(system.size());
+		Map<String, Member> ownerToMembers = system.stream().map(ClassReader::getClassName).collect(Collectors.toMap(Function.identity(), Member::new));
 
 		for (ClassReader reader : system) {
-			Set<String> members = new HashSet<>();
-			Map<String, Set<String>> usedMethods = new HashMap<>();
-			Map<String, Set<String>> usedFields = new HashMap<>();
-
 			reader.accept(new ClassVisitor(Opcodes.ASM9) {
+				private Member self;
+
+				@Override
+				public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+					self = ownerToMembers.get(name);
+				}
+
 				@Override
 				public FieldVisitor visitField(int access, String name, String descriptor, String signature, Object value) {
-					if (Modifier.isPrivate(access)) members.add(name + '#' + descriptor);
+					if (Modifier.isPrivate(access)) self.fields.add(name + '#' + descriptor);
 
 					return null;
 				}
 
 				@Override
 				public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
-					if (Modifier.isPrivate(access)) members.add(name.concat(descriptor));
+					if (Modifier.isPrivate(access)) self.methods.add(name.concat(descriptor));
 
 					return new MethodVisitor(api) {
 						private void visitMethod(String owner, String name, String descriptor) {
-							usedMethods.computeIfAbsent(owner, k -> new HashSet<>()).add(name.concat(descriptor));
+							Member member = ownerToMembers.get(owner);
+
+							if (member != null && member != self) { //Don't log the use of our own methods
+								member.usedMethods.add(name.concat(descriptor));
+							}
 						}
 
 						@Override
 						public void visitFieldInsn(int opcode, String owner, String name, String descriptor) {
-							usedFields.computeIfAbsent(owner, k -> new HashSet<>()).add(name + '#' + descriptor);
+							Member member = ownerToMembers.get(owner);
+
+							if (member != null && member != self) { //Don't log the use of our own fields
+								member.usedFields.add(name + '#' + descriptor);
+							}
 						}
 
 						@Override
@@ -279,40 +293,14 @@ public class BulkRemapper implements IMixinConfigPlugin {
 					};
 				}
 			}, ClassReader.SKIP_FRAMES | ClassReader.SKIP_DEBUG);
-
-			String name = reader.getClassName();
-			ownerToMembers.put(name, new Member(name, members, usedMethods, usedFields));
-		}
-
-		for (Member member : ownerToMembers.values()) {
-			for (Entry<String, Set<String>> entry : member.usedFields.entrySet()) {
-				Member owner = ownerToMembers.get(entry.getKey());
-
-				if (owner != null) {
-					for (String field : entry.getValue()) {
-						if (owner.members.contains(field)) {
-							owner.wantedFields.add(field);
-						}
-					}
-				}
-			}
-			for (Entry<String, Set<String>> entry : member.usedMethods.entrySet()) {
-				Member owner = ownerToMembers.get(entry.getKey());
-
-				if (owner != null) {
-					for (String method : entry.getValue()) {
-						if (owner.members.contains(method)) {
-							owner.wantedMethods.add(method);
-						}
-					}
-				}
-			}
 		}
 
 		List<Runnable> tasks = new ArrayList<>(ownerToMembers.size());
 		for (Member member : ownerToMembers.values()) {
-			Set<String> neededMethods = member.wantedMethods;
-			Set<String> neededFields = member.wantedFields;
+			Set<String> neededMethods = member.usedMethods;
+			neededMethods.retainAll(member.methods);
+			Set<String> neededFields = member.usedFields;
+			neededFields.retainAll(member.fields);
 			if (neededFields.isEmpty() && neededMethods.isEmpty()) continue;
 
 			tasks.add(() -> {
@@ -497,14 +485,14 @@ public class BulkRemapper implements IMixinConfigPlugin {
 						switch (min.name.concat(min.desc)) {
 						case "position(I)Ljava/nio/ByteBuffer;":
 						case "limit(I)Ljava/nio/ByteBuffer;":
-					    case "flip()Ljava/nio/ByteBuffer;":
-					    case "clear()Ljava/nio/ByteBuffer;":
-					    case "mark()Ljava/nio/ByteBuffer;":
-					    case "reset()Ljava/nio/ByteBuffer;":
-					    case "rewind()Ljava/nio/ByteBuffer;":
-					    	min.desc = min.desc.substring(0, min.desc.length() - 11).concat("Buffer;");
-					    	it.add(new TypeInsnNode(Opcodes.CHECKCAST, "java/nio/ByteBuffer"));
-					    	break;
+						case "flip()Ljava/nio/ByteBuffer;":
+						case "clear()Ljava/nio/ByteBuffer;":
+						case "mark()Ljava/nio/ByteBuffer;":
+						case "reset()Ljava/nio/ByteBuffer;":
+						case "rewind()Ljava/nio/ByteBuffer;":
+							min.desc = min.desc.substring(0, min.desc.length() - 11).concat("Buffer;");
+							it.add(new TypeInsnNode(Opcodes.CHECKCAST, "java/nio/ByteBuffer"));
+							break;
 						}
 						break;
 					}
@@ -513,14 +501,14 @@ public class BulkRemapper implements IMixinConfigPlugin {
 						switch (min.name.concat(min.desc)) {
 						case "position(I)Ljava/nio/FloatBuffer;":
 						case "limit(I)Ljava/nio/FloatBuffer;":
-					    case "flip()Ljava/nio/FloatBuffer;":
-					    case "clear()Ljava/nio/FloatBuffer;":
-					    case "mark()Ljava/nio/FloatBuffer;":
-					    case "reset()Ljava/nio/FloatBuffer;":
-					    case "rewind()Ljava/nio/FloatBuffer;":
-					    	min.desc = min.desc.substring(0, min.desc.length() - 12).concat("Buffer;");
-					    	it.add(new TypeInsnNode(Opcodes.CHECKCAST, "java/nio/FloatBuffer"));
-					    	break;
+						case "flip()Ljava/nio/FloatBuffer;":
+						case "clear()Ljava/nio/FloatBuffer;":
+						case "mark()Ljava/nio/FloatBuffer;":
+						case "reset()Ljava/nio/FloatBuffer;":
+						case "rewind()Ljava/nio/FloatBuffer;":
+							min.desc = min.desc.substring(0, min.desc.length() - 12).concat("Buffer;");
+							it.add(new TypeInsnNode(Opcodes.CHECKCAST, "java/nio/FloatBuffer"));
+							break;
 						}
 						break;
 					}
@@ -529,14 +517,14 @@ public class BulkRemapper implements IMixinConfigPlugin {
 						switch (min.name.concat(min.desc)) {
 						case "position(I)Ljava/nio/IntBuffer;":
 						case "limit(I)Ljava/nio/IntBuffer;":
-					    case "flip()Ljava/nio/IntBuffer;":
-					    case "clear()Ljava/nio/IntBuffer;":
-					    case "mark()Ljava/nio/IntBuffer;":
-					    case "reset()Ljava/nio/IntBuffer;":
-					    case "rewind()Ljava/nio/IntBuffer;":
-					    	min.desc = min.desc.substring(0, min.desc.length() - 10).concat("Buffer;");
-					    	it.add(new TypeInsnNode(Opcodes.CHECKCAST, "java/nio/IntBuffer"));
-					    	break;
+						case "flip()Ljava/nio/IntBuffer;":
+						case "clear()Ljava/nio/IntBuffer;":
+						case "mark()Ljava/nio/IntBuffer;":
+						case "reset()Ljava/nio/IntBuffer;":
+						case "rewind()Ljava/nio/IntBuffer;":
+							min.desc = min.desc.substring(0, min.desc.length() - 10).concat("Buffer;");
+							it.add(new TypeInsnNode(Opcodes.CHECKCAST, "java/nio/IntBuffer"));
+							break;
 						}
 						break;
 					}
