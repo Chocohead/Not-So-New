3009768beaa3d0120398587f36148b317c1d16d2
Chocohead <Chocohead@Aroma1997.org>
2022-06-12 18:50:05 +0100

    Maybe fix build?
    Not sure why this isn't happy, but `main` doesn't get obfuscated so this should be fine

--- src/com/chocohead/nsn/mixins/MainMixin.java
+++ src/com/chocohead/nsn/mixins/MainMixin.java
@@ -8,7 +8,7 @@ import net.minecraft.client.main.Main;
 
 @Mixin(Main.class)
 abstract class MainMixin {
-	@ModifyArg(method = "main", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/crash/CrashReport;create(Ljava/lang/Throwable;Ljava/lang/String;)Lnet/minecraft/util/crash/CrashReport;"))
+	@ModifyArg(method = "main", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/crash/CrashReport;create(Ljava/lang/Throwable;Ljava/lang/String;)Lnet/minecraft/util/crash/CrashReport;", remap = true), remap = false)
 	private static Throwable onBang(Throwable t) {
 		t.printStackTrace();
 		return t;
