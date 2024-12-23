da1514c472f7895c0b82e2007e84ae3cd7169f32
Chocohead <Chocohead@Aroma1997.org>
2024-12-23 01:23:05 +0000

    Track type usage by Mixin plugins using Class#forName
    Fixes #26
    Most basic approach for now, expecting the string to be loaded then immediately used
    Method order follows `AnalyzerAdapter` (not that it matters)

--- src/com/chocohead/nsn/TypeSpectator.java
+++ src/com/chocohead/nsn/TypeSpectator.java
@@ -2,11 +2,14 @@ package com.chocohead.nsn;
 
 import org.objectweb.asm.ConstantDynamic;
 import org.objectweb.asm.Handle;
+import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 
 public abstract class TypeSpectator extends MethodVisitor {
+	private String lastString;
+
 	public TypeSpectator() {
 		super(Opcodes.ASM9);
 	}
@@ -31,12 +34,14 @@ public abstract class TypeSpectator extends MethodVisitor {
 	@Override
 	public void visitTypeInsn(int opcode, String type) {
 		checkedVisitType(Type.getObjectType(type));
+		lastString = null;
 	}
 
 	@Override
 	public void visitFieldInsn(int opcode, String owner, String name, String descriptor) {
 		checkedVisitType(Type.getObjectType(owner));
 		checkedVisitType(Type.getType(descriptor));
+		lastString = null;
 	}
 
 	@Override
@@ -44,6 +49,13 @@ public abstract class TypeSpectator extends MethodVisitor {
 		checkedVisitType(Type.getObjectType(owner));
 		for (Type arg : Type.getArgumentTypes(descriptor)) checkedVisitType(arg);
 		checkedVisitType(Type.getReturnType(descriptor));
+
+		if (lastString != null) {
+			if ("java/lang/Class".equals(owner) && "forName".equals(name)) {
+				checkedVisitType(Type.getObjectType(lastString.replace('.', '/')));
+			}
+			lastString = null;
+		}
 	}
 
 	@Override
@@ -52,10 +64,12 @@ public abstract class TypeSpectator extends MethodVisitor {
 		checkedVisitType(Type.getReturnType(descriptor));
 		visitHandle(bootstrapMethodHandle);
 		for (Object arg : bootstrapMethodArguments) visitLdcInsn(arg);
+		lastString = null;
 	}
 
 	@Override
 	public void visitLdcInsn(Object value) {
+		lastString = null;
 		if (value instanceof Type) {
 			Type type = (Type) value;
 
@@ -81,6 +95,8 @@ public abstract class TypeSpectator extends MethodVisitor {
 			checkedVisitType(Type.getType(constant.getDescriptor()));
 			visitHandle(constant.getBootstrapMethod());
 			for (int i = 0, end = constant.getBootstrapMethodArgumentCount(); i < end; i++) visitLdcInsn(constant.getBootstrapMethodArgument(i));
+		} else if (value instanceof String) {
+			lastString = (String) value;
 		}
 	}
 
@@ -112,5 +128,50 @@ public abstract class TypeSpectator extends MethodVisitor {
 	@Override
 	public void visitMultiANewArrayInsn(String descriptor, int numDimensions) {
 		checkedVisitType(Type.getType(descriptor).getElementType());
+		lastString = null;
+	}
+
+	@Override
+	public void visitFrame(int type, int numLocal, Object[] local, int numStack, Object[] stack) {
+		if (type != Opcodes.F_SAME1) {
+			lastString = null;
+		}
+	}
+
+	@Override
+	public void visitInsn(int opcode) {
+		if (opcode != Opcodes.NOP) {
+			lastString = null;
+		}
+	}
+
+	@Override
+	public void visitIntInsn(int opcode, int operand) {
+		lastString = null;
+	}
+
+	@Override
+	public void visitVarInsn(int opcode, int varIndex) {
+		lastString = null;
+	}
+
+	@Override
+	public void visitJumpInsn(int opcode, Label label) {
+		lastString = null;
+	}
+
+	@Override
+	public void visitIincInsn(int varIndex, int increment) {
+		lastString = null;
+	}
+
+	@Override
+	public void visitTableSwitchInsn(int min, int max, Label dflt, Label... labels) {
+		lastString = null;
+	}
+
+	@Override
+	public void visitLookupSwitchInsn(Label dflt, int[] keys, Label[] labels) {
+		lastString = null;
 	}
 }
