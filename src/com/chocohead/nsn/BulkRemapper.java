Chocohead/Not-So-New

commit e757d75a364cc03907d9e1b45a1f7c2c04fd9306

Remap HTTP uses in method handles
Fixes WTHIT's PetOwnerProvider

build.gradle
@@ -21,7 +21,7 @@ sourceCompatibility = 1.8
 targetCompatibility = 1.8
 
 archivesBaseName = 'NotSoNew'
-version = '6.26'
+version = '6.27'
 
 repositories {
 	maven { 

src/com/chocohead/nsn/BulkRemapper.java
@@ -552,6 +552,13 @@ public class BulkRemapper implements IMixinConfigPlugin {
 								idin.bsmArgs[i] = new Handle(handle.getTag(), "com/chocohead/nsn/StackWalker$StackFrame", handle.getName(), handle.getDesc(), handle.isInterface());
 								break;
 							}
+
+							default: {
+								if (handle.getOwner().startsWith("java/net/http/")) {
+									idin.bsmArgs[i] = new Handle(handle.getTag(), handle.getOwner().replace("java/net/http/", "com/chocohead/nsn/http/"), handle.getName(), handle.getDesc().replace("Ljava/net/http/", "Lcom/chocohead/nsn/http/"), handle.isInterface());
+								}
+								break;
+							}
 							}
 						} else if (idin.bsmArgs[i] instanceof Type) {
 							Type type = (Type) idin.bsmArgs[i];
@@ -576,8 +583,8 @@ public class BulkRemapper implements IMixinConfigPlugin {
 										madeChange = true;
 									}
 								}
-								if (returnType.getDescriptor().contains("Ljava/lang/Record;") || returnType.getDescriptor().contains("Ljava/util/ServiceLoader$Provider;")) {
-									returnType = Type.getType(returnType.getDescriptor().replace("Ljava/lang/Record;", "Ljava/lang/Object;").replace("Ljava/util/ServiceLoader$Provider;", "Lcom/chocohead/nsn/ServiceLoaders$Provider;"));
+								if ("java/lang/Record".equals(returnType.getInternalName()) || "Ljava/util/ServiceLoader$Provider;".equals(returnType.getInternalName()) || returnType.getInternalName().startsWith("java/net/http/")) {
+									returnType = Type.getType(returnType.getDescriptor().replace("Ljava/lang/Record;", "Ljava/lang/Object;").replace("Ljava/util/ServiceLoader$Provider;", "Lcom/chocohead/nsn/ServiceLoaders$Provider;").replace("Ljava/net/http/", "Lcom/chocohead/nsn/http/"));
 									madeChange = true;
 								}
 

commit c993a38a70bdc2be500e8ee60128b806be219b49

Support 25w04a
Adds some more superficial type support for `SequencedSet` 
Also fixes a bug introduced by e757d75 relating to method handles returning (`ServiceLoader`) `Provider`s

build.gradle
@@ -21,7 +21,7 @@ sourceCompatibility = 1.8
 targetCompatibility = 1.8
 
 archivesBaseName = 'NotSoNew'
-version = '6.48'
+version = '6.49'
 
 repositories {
 	maven { 

src/com/chocohead/nsn/BulkRemapper.java
@@ -474,9 +474,9 @@ public class BulkRemapper implements IMixinConfigPlugin {
 						if (idin.bsmArgs[i] instanceof Handle) {
 							Handle handle = (Handle) idin.bsmArgs[i];
 
-							if (handle.getDesc().contains("Ljava/lang/Record;") || handle.getDesc().contains("Ljava/util/ServiceLoader$Provider;")) {
+							if (handle.getDesc().contains("Ljava/lang/Record;") || handle.getDesc().contains("Ljava/util/ServiceLoader$Provider;") || handle.getDesc().contains("Ljava/util/SequencedSet;")) {
 								idin.bsmArgs[i] = handle = new Handle(handle.getTag(), handle.getOwner(), handle.getName(),
-										handle.getDesc().replace("Ljava/lang/Record;", "Ljava/lang/Object;").replace("Ljava/util/ServiceLoader$Provider;", "Lcom/chocohead/nsn/ServiceLoaders$Provider;"), handle.isInterface());
+										handle.getDesc().replace("Ljava/lang/Record;", "Ljava/lang/Object;").replace("Ljava/util/ServiceLoader$Provider;", "Lcom/chocohead/nsn/ServiceLoaders$Provider;").replace("Ljava/util/SequencedSet;", "Ljava/util/Set;"), handle.isInterface());
 							}
 
 							switch (handle.getOwner()) {
@@ -597,15 +597,30 @@ public class BulkRemapper implements IMixinConfigPlugin {
 
 								for (int j = 0; j < args.length; j++) {
 									String desc = args[j].getDescriptor();
-									if (desc.contains("Ljava/lang/Record;") || desc.contains("Ljava/util/ServiceLoader$Provider;") || desc.contains("Ljava/lang/StackWalker$StackFrame;") || desc.contains("Ljava/net/http/")) {
+									if (desc.contains("Ljava/lang/Record;") || desc.contains("Ljava/util/ServiceLoader$Provider;") || desc.contains("Ljava/lang/StackWalker$StackFrame;") || desc.contains("Ljava/net/http/") || desc.contains("Ljava/util/SequencedSet;")) {
 										args[j] = Type.getType(desc.replace("Ljava/lang/Record;", "Ljava/lang/Object;").replace("Ljava/util/ServiceLoader$Provider;", "Lcom/chocohead/nsn/ServiceLoaders$Provider;")
-												.replace("Ljava/lang/StackWalker$StackFrame;", "Lcom/chocohead/nsn/StackWalker$StackFrame;").replace("Ljava/net/http/", "Lcom/chocohead/nsn/http/"));
+												.replace("Ljava/lang/StackWalker$StackFrame;", "Lcom/chocohead/nsn/StackWalker$StackFrame;").replace("Ljava/net/http/", "Lcom/chocohead/nsn/http/").replace("Ljava/util/SequencedSet;", "Ljava/util/Set;"));
 										madeChange = true;
 									}
 								}
-								if ("java/lang/Record".equals(returnType.getInternalName()) || "Ljava/util/ServiceLoader$Provider;".equals(returnType.getInternalName()) || returnType.getInternalName().startsWith("java/net/http/")) {
-									returnType = Type.getType(returnType.getDescriptor().replace("Ljava/lang/Record;", "Ljava/lang/Object;").replace("Ljava/util/ServiceLoader$Provider;", "Lcom/chocohead/nsn/ServiceLoaders$Provider;").replace("Ljava/net/http/", "Lcom/chocohead/nsn/http/"));
+								switch (returnType.getInternalName()) {
+								case "java/lang/Record":
+									returnType = Type.getObjectType("java/lang/Object");
 									madeChange = true;
+									break;
+								case "java/util/ServiceLoader$Provider":
+									returnType = Type.getObjectType("com/chocohead/nsn/ServiceLoaders$Provider");
+									madeChange = true;
+									break;
+								case "java/util/SequencedSet":
+									returnType = Type.getObjectType("java/util/Set");
+									madeChange = true;
+									break;
+								default:
+									if (returnType.getInternalName().startsWith("java/net/http/")) {
+										returnType = Type.getType(returnType.getDescriptor().replace("Ljava/net/http/", "Lcom/chocohead/nsn/http/"));
+										madeChange = true;
+									}
 								}
 
 								if (madeChange) idin.bsmArgs[i] = Type.getMethodType(returnType, args);
@@ -1874,7 +1889,7 @@ public class BulkRemapper implements IMixinConfigPlugin {
 					fin.desc = fin.desc.replace("java/lang/StackWalker", "com/chocohead/nsn/StackWalker").replace("java/lang/System$Logger", "com/chocohead/nsn/SystemLogger")
 							.replace("java/util/SequencedMap", "java/util/Map").replace("java/net/http/", "com/chocohead/nsn/http/").replace("java/lang/Record", "java/lang/Object")
 							.replace("java/lang/invoke/MethodHandles$Lookup$ClassOption", "com/chocohead/nsn/Binoculars$ClassOption").replace("java/lang/invoke/VarHandle", "com/chocohead/nsn/VarHandle")
-							.replace("java/util/HexFormat", "com/chocohead/nsn/Hexed");
+							.replace("java/util/HexFormat", "com/chocohead/nsn/Hexed").replace("java/util/SequencedSet", "java/util/Set");
 
 					if ("java/lang/StackWalker$Option".equals(fin.owner)) {
 						fin.owner = "com/chocohead/nsn/StackWalker$Option";
@@ -1995,7 +2010,7 @@ public class BulkRemapper implements IMixinConfigPlugin {
 		for (FieldNode field : node.fields) {
 			field.desc = field.desc.replace("java/lang/StackWalker", "com/chocohead/nsn/StackWalker").replace("java/lang/System$Logger", "com/chocohead/nsn/SystemLogger")
 					.replace("java/lang/Record", "java/lang/Object").replace("java/util/SequencedMap", "java/util/Map").replace("java/net/http/", "com/chocohead/nsn/http/")
-					.replace("java/lang/invoke/VarHandle", "com/chocohead/nsn/VarHandle").replace("java/util/HexFormat", "com/chocohead/nsn/Hexed");
+					.replace("java/lang/invoke/VarHandle", "com/chocohead/nsn/VarHandle").replace("java/util/HexFormat", "com/chocohead/nsn/Hexed").replace("java/util/SequencedSet", "java/util/Set");
 		}
 
 		for (Iterator<InnerClassNode> it = node.innerClasses.iterator(); it.hasNext();) {